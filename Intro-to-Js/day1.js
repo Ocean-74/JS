313bf3fe19b85237f713499843f17ca55daaa77c
Bansari <bansari16vaishnav11@gmail.com>
2026-01-03 03:03:39 +0530

    Re-add intro to JavaScript practice

---
+++ Intro-to-Js/day1.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b="i'm trying to be good";
+let c=true;
+let d;
+let e= {name:'bnsri', age:20}
+const max = 100;
+const hy="im oky";
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(max)
+console.log(hy)
+a=34;
+console.log(a)
