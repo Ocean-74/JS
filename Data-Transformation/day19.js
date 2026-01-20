c08faec4996379a30fa1e4dadd41de0717cd0788
Bansari <bansari16vaishnav11@gmail.com>
2026-01-21 00:44:14 +0530

    Day 19 : Data transformation using map reduce and filter

--- Data-Transformation/day19.js
+++ Data-Transformation/day19.js
@@ -32,3 +32,24 @@ let result = users.map(user => {
 });
 
 console.log(result);
+
+let names = users
+    .filter(user=>user.profile.isAdult && user.login.isLoggedIn)
+    .map(user => user.name)
+console.log(names);
+
+let summary = users.reduce((acc,user)=>{
+    acc.totalUsers++
+    if(user.profile.isAdult){
+        acc.adults++
+    }else{
+        acc.minors++
+    }
+    if(user.login.isLoggedIn){
+        acc.loggedIn++
+    }
+    if(user.login.isLoggedIn && user.profile.isAdult){
+        acc.loggedInAdults++
+    }return acc;
+},{totalUsers:0,adults:0,minors:0,loggedIn:0,loggedInAdults:0})
+console.log(summary);
