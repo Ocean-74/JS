c9786ecf962db744d09a475d52343feddeef9ea8
Bansari <bansari16vaishnav11@gmail.com>
2026-01-23 00:10:49 +0530

    Day 21 : Remove derivable things from user data

--- Data-Transformation/day21.js
+++ Data-Transformation/day21.js
@@ -2,28 +2,45 @@ let users = [
   {
     id: 1,
     name: "A",
-    profile: { age: 20, isAdult: true },
+    profile: { age: 20 },
     login: { isLoggedIn: true }
   },
   {
     id: 2,
     name: "B",
-    profile: { age: 17, isAdult: false },
+    profile: { age: 17 },
     login: { isLoggedIn: false }
   },
   {
     id: 3,
     name: "C",
-    profile: { age: 25, isAdult: true },
+    profile: { age: 25 },
     login: { isLoggedIn: true }
   },
   {
     id: 4,
     name: "D",
-    profile: { age: 16, isAdult: false },
+    profile: { age: 16 },
     login: { isLoggedIn: true }
   }
 ];
+
+function isAdult(age){
+    let status = age>= 18? "true" : "false" ; 
+    return status
+}
+function updateUserAge(id,newAge){
+    for(user of users){
+        if(user.id===id){
+            user.profile.age = newAge;
+            user.profile.isAdult = user.profile.age >= 18? true : false ;
+            return "updated";
+        }
+    }
+    return "User Not found";
+}
+console.log(updateUserAge(2,18));
+console.log(updateUserAge(99, 20));
 
 function formatUser(user){
         let group = user.profile.age>=18 ? "adult" : "minor" ;
@@ -32,21 +49,9 @@ function formatUser(user){
 }
 console.log(formatUser(users[2]));
 
-function updateUserAge(id, newAge){
-    for(user of users){
-        if(user.id === id){
-            user.profile.age = newAge;
-            user.profile.isAdult = newAge >= 18
-            return "Updated";
-        }
-    }
-    return "User not Found!!"
-}
-console.log(updateUserAge(4,21));
-
 let summary = users.reduce((acc,user)=>{
     acc.totalUsers++;
-    if(user.profile.isAdult=== true){
+    if(user.profile.age>=18){
         acc.adults++
     }else{
         acc.minors++
@@ -54,7 +59,7 @@ let summary = users.reduce((acc,user)=>{
     if(user.login.isLoggedIn===true){
         acc.loggedIn++;
     }
-    if(user.login.isLoggedIn===true && user.profile.isAdult===true){
+    if(user.login.isLoggedIn===true && user.profile.age >=18){
         acc.loggedInAdults++;
     }return acc;
 },{totalUsers:0,adults:0,minors:0,loggedIn:0,loggedInAdults:0});
